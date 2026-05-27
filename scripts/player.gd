3fdec86e320e230ab553e4f9086b674f902fb265
Chetan Seervi <chetanseervi513@gmail.com>
2026-05-27 15:37:01 +0530

    Add animated sprite with directional animations and animation logic

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,25 @@
 extends CharacterBody2D
 
+
 var speed := 150
 var direction := Vector2.ZERO
 
+
 func _physics_process(_delta: float):
 	direction = Input.get_vector("left", "right", "up", "down")
+	
 	self.velocity = direction * speed
+	animation()
 	self.move_and_slide()
+
+
+func animation():
+	if (direction):
+		$AnimatedSprite2D.flip_h = direction.x > 0
+		
+		if (direction.x != 0):
+			$AnimatedSprite2D.animation = "left"
+		else:
+			$AnimatedSprite2D.animation = "up" if (direction.y < 0) else "down"
+	else:
+		$AnimatedSprite2D.frame = 0
